48a8104976f34113c557dbd6b17598fec8eb7fbe
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2019-01-24 22:54:24 +0900

    escape html

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
 const code = `
 (() => {
+  const escapeHtml = (unsafeString) => {
+    return unsafeString
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  };
   let input = document.createElement("input");
-  input.value = '[' + document.title + '](' + window.location.href + ')';
+  input.value = '[' + escapeHtml(document.title) + '](' + window.location.href + ')';
   document.body.appendChild(input);
   input.select();
   console.log(document.execCommand('copy'));
